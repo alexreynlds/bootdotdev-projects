dae0331f484e8ad4244119e184acb07f7b563f1d
alexreynlds <ajrey@live.co.uk>
2026-04-29 22:55:16 +0100

    Added splitting for non minimal sized asteroids

--- asteroids/asteroid.py
+++ asteroids/asteroid.py
@@ -1,6 +1,8 @@
+import random
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -12,3 +14,22 @@ class Asteroid(CircleShape):
 
     def update(self, delta_time):
         self.position += self.velocity * delta_time
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+
+        first_child_velocity = self.velocity.rotate(random_angle)
+        second_child_velocity = self.velocity.rotate(-random_angle)
+
+        child_radius = self.radius - ASTEROID_MIN_RADIUS
+        child1 = Asteroid(self.position.x, self.position.y, child_radius)
+        child2 = Asteroid(self.position.x, self.position.y, child_radius)
+
+        child1.velocity = first_child_velocity * 1.2
+        child2.velocity = second_child_velocity * 1.2
--- asteroids/main.py
+++ asteroids/main.py
@@ -55,7 +55,7 @@ def main():
                 if is_colliding:
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         for item in drawable:
             item.draw(screen)
