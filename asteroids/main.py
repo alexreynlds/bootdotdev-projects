d2c1ed87e1d4822439d18d8b8be885652eb11d79
alexreynlds <ajrey@live.co.uk>
2026-04-29 21:10:37 +0100

    Began adding asteroids and added asteroid field background

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
@@ -10,7 +12,16 @@ def main():
     delta_time = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -21,8 +32,11 @@ def main():
 
         screen.fill("black")
 
-        player.update(delta_time)
-        player.draw(screen)
+        for item in updatable:
+            item.update(delta_time)
+
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
 
